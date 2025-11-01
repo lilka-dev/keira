2975ada7d441fd6625769d4c8b195ff0c04c5bf5
Alex <15167344+frostmorn@users.noreply.github.com>
2025-11-01 13:13:08 +0200

    noffs/doffs inital calculation in textviewer

--- src/apps/txtviewer/txtview.cpp
+++ src/apps/txtviewer/txtview.cpp
@@ -2,43 +2,69 @@
 #include <errno.h>
 
 // move to next Unicode character
-const char *uforward(const char *cstr) {
+static inline char* uforward(char* cstr) {
+    // TENTER
     if (!cstr || !*cstr) return cstr;
     cstr++;
-    while ((*cstr & 0xC0) == 0x80) cstr++; // skip continuation bytes
+    while ((*cstr & 0xC0) == 0x80)
+        cstr++; // skip continuation bytes
     return cstr;
 }
 
 // move to beginning of previous Unicode character
-const char *ubackward(const char *cstr) {
+static inline char* ubackward(char* cstr) {
+    // TENTER
     if (!cstr) return cstr;
-    const char *p = cstr - 1;
-    while ((*(unsigned char*)p & 0xC0) == 0x80) --p; // skip continuation bytes
+    char* p = cstr - 1;
+    while ((*(unsigned char*)p & 0xC0) == 0x80)
+        --p; // skip continuation bytes
     return p;
+}
+// get length of a first Unicode character
+static inline size_t ulen(char* cstr) {
+    auto nextchar = uforward(cstr);
+    return nextchar - cstr;
+}
+// expects bounds and stuff to be set
+bool isLineWithinCanvas(char *pLine, Arduino_GFX *canvas) {
+    if (!pLine || !canvas) return false;
+
+    int16_t x1, y1;
+    uint16_t w, h;
+
+    // Measure text bounds at (0,0)
+    canvas->getTextBounds(pLine, 0, 0, &x1, &y1, &w, &h);
+
+    // Get current display size
+    int16_t cw = canvas->width();
+    int16_t ch = canvas->height();
+
+    // If it fits horizontally (and optionally vertically)
+    return (w <= cw && h <= ch);
 }
 
 // gets offset to begining of previous line
-long flineback(FILE *fp, long maxBack){
-    long initalPos = ftell(fp); 
+long flineback(FILE* fp, long maxBack) {
+    TENTER
+    long initalPos = ftell(fp);
     long curPos = initalPos;
     int newLineCount = 0;
     int c;
 
-    while (curPos > 0){
+    while (curPos > 0) {
         fseek(fp, --curPos, SEEK_SET);
-        if (initalPos - curPos > maxBack){ // can't go back
-            fseek(fp, initalPos, SEEK_SET);        
+        if (initalPos - curPos > maxBack) { // can't go back
+            fseek(fp, initalPos, SEEK_SET);
             return -1;
         }
         c = fgetc(fp);
-        if (c == '\n'){
+        if (c == '\n') {
             newLineCount++;
-            if (newLineCount == 2){  // first \n to begining of line, previous to begining of previous one
+            if (newLineCount == 2) { // first \n to begining of line, previous to begining of previous one
                 curPos++;
                 break;
             }
         }
-
     }
     if (newLineCount < 2) curPos = 0;
     fseek(fp, initalPos, SEEK_SET);
@@ -47,89 +73,195 @@ long flineback(FILE *fp, long maxBack){
 }
 
 TxtView::TxtView() {
-
+    TENTER
 }
 
-bool TxtView::isFinished(){
+bool TxtView::isFinished() {
+    // TENTER
     // TODO: move to ABSTRACT WIDGET
     if (done) {
         done = false;
         return true;
     }
     return false;
-
 }
 // TODO: PSRAM VFS for other cases?
-void TxtView::setTextFile(const String &fPath){
-    if (fp){ // close old file
+void TxtView::setTextFile(const String& fPath) {
+    TENTER
+    if (fp) { // close old file
         fclose(fp);
         fp = NULL;
     }
     fp = fopen(fPath.c_str(), "r");
-    if (fp == NULL){
+    if (fp == NULL) {
         lilka::serial.err("Tried to open file %s. Errno %d", fPath.c_str(), errno);
     }
-    tBlockRefreshRequired = true;   // to be done in update()
+    TXT_DBG lilka::serial.log("Set file to %s", fPath.c_str());
+    tBlockRefreshRequired = true; // to be done in update()
 }
 
-void TxtView::update(){
+void TxtView::update() {
+    // TENTER
     updateKeys();
-    if (tBlockRefreshRequired){
+    // We've to check if we actually can prepare data
+    // Cause we've no real idea about canvas dimensions
+    // it could be impossible to do
+    // We still can read file though, but for simplification we 
+    // just wait a few cycles
+    if (lastCanvas && tBlockRefreshRequired) {
         tBlockRefresh();
         nOffsRefresh();
-        dOffsRefresh();        
+        dOffsRefresh();
     }
 }
 
-void TxtView::updateKeys(){
+void TxtView::updateKeys() {
+    // TENTER
     auto state = lilka::controller.getState();
-    if (state.up.justPressed)
-        scrollUp();
-    else if (state.down.justPressed)
-        scrollDown();
-    else if (state.left.justPressed)
-        scrollPageUp();
-    else if (state.right.justPressed)
-        scrollPageDown();
-    else if (state.b.justPressed)
-        done = true;
+    if (state.up.justPressed) scrollUp();
+    else if (state.down.justPressed) scrollDown();
+    else if (state.left.justPressed) scrollPageUp();
+    else if (state.right.justPressed) scrollPageDown();
+    else if (state.b.justPressed) done = true;
 }
 
-void TxtView::draw(Arduino_GFX *canvas){
+void TxtView::draw(Arduino_GFX* canvas) {
+    // TENTER
+    setCanvasOptions(canvas);
+    lastCanvas = canvas;
+    
+    // Doesn't look like we've something to display
+    // could be a first run, so we have no data prepared
+    if (doffs.empty())
+        return;
+
     // display doffs
 
     // sleep
 }
 
-void TxtView::tBlockRefresh(){
-    if (fp == NULL)
+void TxtView::tBlockRefresh() {
+    TENTER
+    if (fp == NULL) {
+        TXT_DBG lilka::serial.log("File not open. Skiping");
         return; // nothing to refresh. Wait till next update
+    }
 
     // Read text block
-    fread(tBlock, sizeof(tBlock), 1, fp);
+    long curPos = ftell(fp);
+    tLen = fread(tBlock, 1, TXT_MAX_BLOCK_SIZE, fp);
+    fseek(fp, curPos, SEEK_SET); // save original file offset, could be useful for refresh
     tBlockRefreshRequired = false;
-}
-void TxtView::nOffsRefresh(){
 
-}
-void TxtView::dOffsRefresh(){
-    // TODO: invent a good way to store canvas options
+    TXT_DBG lilka::serial.log("Read %d bytes", tLen);
 }
 
-void TxtView::scrollUp(){
-    tBlockRefreshRequired = true;   // to be done in update()
+void TxtView::nOffsRefresh() {
+    TENTER
+    noffs.clear();
+    if (!tLen) // Nothing read
+        return;
+    char* pCurrentChar = tBlock;
+    char* pEndBlock = tBlock + tLen;
+    noffs.push_back(pCurrentChar);
+    // do not check last character here
+    for (; pCurrentChar < pEndBlock; pCurrentChar++) {
+        if (*pCurrentChar == '\n') {
+            noffs.push_back(pCurrentChar + 1);
+            TXT_DBG lilka::serial.log("Adding noff at %p\n", noffs.back());
+        }
+    }
+    TXT_DBG lilka::serial.log("Added %d noffs \n", noffs.size());
 }
-void TxtView::scrollDown(){
-    tBlockRefreshRequired = true;   // to be done in update()
+
+void TxtView::dOffsRefresh() {
+    TENTER
+    // assume canvas options are already set
+    if (!lastCanvas){
+        TXT_DBG lilka::serial.err("No access to lastCanvas, can't calc doffs");
+        tBlockRefreshRequired = true; // reread then
+        return;
+    }
+    // iterate by actual lines
+    for (auto noff:noffs){
+        doffs.push_back(noff); // append line begining
+        // now we've to move from noff till fits screen, or next noff
+        char *pLineStart = noff;
+        char *pLineEnd = uforward(pLineStart); // first letter
+        while (pLineEnd != tBlock + tLen){    // fit bounds
+            char backupChar = *pLineEnd; 
+            // Of course we can use temporary string for that, but why to mess
+            // with a memory. Instead we could insert our null terminator where it's 
+            // needed to send in functions to measure, print what we need to restore it 
+            // backwards. Moving one byte is much easier than a whole stuff
+            *pLineEnd = '\0'; 
+
+            // reached newline
+            if (*(pLineEnd-1) == '\n')
+            {
+                doffs.push_back(pLineStart);
+                TXT_DBG lilka::serial.log("Adding doff at %p\n", doffs.back());
+                // Restore old char
+                *pLineEnd = backupChar;    
+                break; // swap to next noff
+            }
+
+            // Check if it fits
+            if (!isLineWithinCanvas(pLineStart, lastCanvas))
+            {
+                doffs.push_back(pLineStart);    // push current, go next
+                TXT_DBG lilka::serial.log("Adding doff at %p\n", doffs.back());
+                pLineStart = pLineEnd;
+            }             
+
+            // Restore old char
+            *pLineEnd = backupChar;
+            // Go to next unicode character
+            pLineEnd = uforward(pLineEnd);
+        }
+    }
+
 }
-void TxtView::scrollPageUp(){
-    tBlockRefreshRequired = true;   // to be done in update()
+
+void TxtView::scrollUp() {
+    TENTER
+    tBlockRefreshRequired = true; // to be done in update()
 }
-void TxtView::scrollPageDown(){
-    tBlockRefreshRequired = true;   // to be done in update()
+
+void TxtView::scrollDown() {
+    TENTER
+    tBlockRefreshRequired = true; // to be done in update()
 }
-TxtView::~TxtView(){
+
+void TxtView::scrollPageUp() {
+    TENTER
+    tBlockRefreshRequired = true; // to be done in update()
+}
+
+void TxtView::scrollPageDown() {
+    TENTER
+    tBlockRefreshRequired = true; // to be done in update()
+}
+
+TxtView::~TxtView() {
+    TENTER
     // Never forget to close file
-    if (fp)
-        fclose(fp);
+    if (fp) fclose(fp);
+}
+
+void TxtView::setColor(uint16_t color) {
+    this->color = color;
+}
+
+void TxtView::setBgColor(uint16_t bgColor) { this -> bgColor = bgColor; };
+
+void TxtView::setFont(const uint8_t* font) {
+    this->font = font;
+}
+
+void TxtView::setCanvasOptions(Arduino_GFX *canvas){
+    canvas->fillScreen(bgColor);
+    canvas->setTextColor(color);
+    canvas->setTextWrap(false);
+    canvas->setFont(font);
 }
--- src/apps/txtviewer/txtview.h
+++ src/apps/txtviewer/txtview.h
@@ -3,35 +3,70 @@
 #define TXT_MAX_BLOCK_SIZE 2048
 // To be moved in lilka sdk
 
-// 
+//
+#define TXT_VIEWER_DEBUG
+
+#ifdef TXT_VIEWER_DEBUG
+#    define TXT_DBG if (1)
+#else
+#    define TXT_DBG if (0)
+#endif
+// TODO: Move to keira/macro sdk?
+#define TENTER TXT_DBG lilka::serial.log("==> %s:%d %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+#define TEND   TXT_DBG lilka::serial.log("<== %s:%d %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__);
 
 
-class TxtView{
+class TxtView {
 public:
     TxtView();
     bool isFinished();
-    void setTextFile(const String &fPath);    
+    void setTextFile(const String& fPath);
     void update();
     void draw(Arduino_GFX* canvas);
     ~TxtView();
+
 private:
-    void tBlockRefresh();   // read text block, prepare noffs and doffs
+    // Positioning and file read
+    void tBlockRefresh(); // read text block, prepare noffs and doffs
     void nOffsRefresh();
     void dOffsRefresh();
 
+    // Scrolling
     void scrollUp();
     void scrollDown();
     void scrollPageUp();
     void scrollPageDown();
-    
+
+    // Input
     void updateKeys();
 
-    FILE *fp = NULL;
-    long tOffset = 0;
-    char tBlock[TXT_MAX_BLOCK_SIZE];
-    bool tBlockRefreshRequired = true; // inital value
-    std::vector <char *> noffs;  // offsets to actual lines
-    std::vector <char *> doffs;  // offsets to displayed lines
+    // Display options
+    void setColor(uint16_t color);
+    void setBgColor(uint16_t bgColor);
+    void setFont(const uint8_t* font);
 
-    bool done = false;          // TODO: move to ABSTRACT WIDGET
+    // Canvas setup
+    void setCanvasOptions(Arduino_GFX *canvas);
+
+    // File stuff
+    FILE* fp = NULL;
+    long tOffset = 0;
+
+    // Text data
+    char tBlock[TXT_MAX_BLOCK_SIZE];
+    size_t tLen = 0; // text block length
+    bool tBlockRefreshRequired = true; // inital value
+    
+    std::vector<char*> noffs; // offsets to actual lines[separated by \n]
+    std::vector<char*> doffs; // offsets to displayed lines
+
+    // UI options
+    bool done = false; // TODO: move to ABSTRACT WIDGET
+    uint16_t color = lilka::colors::White;
+    uint16_t bgColor = lilka::colors::Black;
+    const uint8_t* font = FONT_9x15;
+
+    // we need somehow calculate how much lines/characters fit display
+    // so we store last canvas pointer to be acessible in dOffsRefresh
+    Arduino_GFX *lastCanvas = NULL; 
 };
