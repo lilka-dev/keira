853f76aeb2817999bd0b51e0590e8f1e46722795
Alex <15167344+frostmorn@users.noreply.github.com>
2025-11-21 12:13:39 +0200

    updates to txtviewer app skelethon

--- src/apps/txtviewer/txtviewer.cpp
+++ src/apps/txtviewer/txtviewer.cpp
@@ -1,9 +1,41 @@
 #include "txtviewer.h"
 
+
+// ===== CONFIGURATION
 TxtViewerApp::TxtViewerApp(String fPath) : App("Text Viewer") {
+    // Configure TxtViewer
+    tView.setCallback(TXTV_CALLBACK_CAST(onTxtViewerButton), TXTV_CALLBACK_PTHIS);
+
+    // Configure Toolbar
+    // Note: should be in order specified in TxtToolBarPage enum
+    toolBar.addPage(TXTV_TOOLBAR_CALLBACK(onGetStrProgress), TXTV_CALLBACK_PTHIS);
+    toolBar.addPage(TXTV_TOOLBAR_CALLBACK(onGetStrFontSize), TXTV_CALLBACK_PTHIS);
+    toolBar.addPage(TXTV_TOOLBAR_CALLBACK(onGetStrFontSpacing), TXTV_CALLBACK_PTHIS);
+
+    
     tView.setTextFile(fPath);
 }
+// ===== END CONFIGURATION
 
+void TxtViewerApp::onTxtViewerButton(){
+    auto aButton = tView.getButton();
+    if (aButton == K_BTN_BACK)
+        return;
+    
+    if (aButton == lilka::Button::D)
+        toolBar.nextPage();
+    
+    if (aButton == lilka::Button::A); // inc page val
+    // TODO: switch across TxtToolBarPage enum, depending on page do different stuff
+
+    if (aButton == lilka::Button::C); // dec page val
+    // TODO: switch across TxtToolBarPage enum, depending on page do different stuff
+
+
+    tView.isFinished(); // clear finished flag
+}
+
+// ===== REDRAW
 void TxtViewerApp::run() {
     while (!tView.isFinished()) {
         tView.update();
@@ -14,5 +46,53 @@ void TxtViewerApp::run() {
 
 void TxtViewerApp::queueDraw() {
     // show control buttons
+    toolBar.update();
+    toolBar.draw(canvas);
     App::queueDraw();
 }
+// ===== END REDRAW
+
+// ===== FONT SETTINGS
+
+void TxtViewerApp::fontSizeInc(){
+    // TODO: font scaling to implement via changing font
+    // it would allow more precise scaling without big size jumps
+}
+
+void TxtViewerApp::fontSizeDec(){
+    // TODO: font scaling to implement via changing font
+    // it would allow more precise scaling without big size jumps
+}
+
+void TxtViewerApp::fontSpacingInc(){
+
+}
+
+void TxtViewerApp::fontSpacingDec(){
+
+}
+
+// ===== END FONT SETTINGS
+
+// ===== TOOLBAR
+
+// char * maybe? and static buffer, should be cool :)
+String TxtViewerApp::onGetStrProgress(){
+    // [===========      ] 80%
+    auto max = tView.getFileSize();
+    auto curPos = tView.getOffset();
+
+    auto piece = curPos/max;
+
+
+
+
+}
+
+String TxtViewerApp::onGetStrFontSize(){
+    // SIZE: 10  D(-) A(+)
+}
+String TxtViewerApp::onGetStrFontSpacing(){
+    // SPACE: 10  D(-) A(+)
+}
+// ===== END TOOLBAR
--- src/apps/txtviewer/txtviewer.h
+++ src/apps/txtviewer/txtviewer.h
@@ -1,14 +1,52 @@
 #pragma once
 #include "keira/app.h"
+#include "keira/keira.h"
 #include "txtview.h"
+#include "toolbar.h"
+
+#define TXTV_CALLBACK_CAST(X)   reinterpret_cast<PTXTViewCallback>(&TxtViewerApp::X)
+#define TXTV_CALLBACK_PTHIS     reinterpret_cast<void*>(this) // K_CLBK_PTHIS?
+#define TXTV_TOOLBAR_CALLBACK(X)   reinterpret_cast<PGetStrCallback>(&TxtViewerApp::X)
+#define TXTV_PROGRESS_LEN 10
+
+#define TXTV_MAX_SPACING 10 // px
+
+
+
+typedef enum{
+    TXTV_PROGRESS,
+    TXTV_FONT_SIZE,
+    TXTV_FONT_SPACING
+} TxtToolBarPage;
 
 class TxtViewerApp : public App {
 public:
     explicit TxtViewerApp(String fPath);
     TxtView tView;
+    ToolBarWidget toolBar;
     void queueDraw();
 
 private:
     TxtViewerApp();
     void run() override;
+
+    // font settings routines
+    void fontSizeInc();
+    void fontSizeDec();
+    void fontSpacingInc();
+    void fontSpacingDec();
+
+    // tView callbacks:
+    void onTxtViewerButton();
+
+    // toolbar callbacks:
+    String onGetStrProgress();
+    String onGetStrFontSize();
+    String onGetStrFontSpacing();
+
+
+    // scale = (font size / font index)
+    // font index should be a list of fonts 
+    uint8_t fontSize;
+    const uint8_t* fonts[8] = {FONT_5x7, FONT_5x8, FONT_6x12, FONT_6x13, FONT_7x13, FONT_8x13, FONT_9x15, FONT_10x20};
 };
