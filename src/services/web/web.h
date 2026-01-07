1cd18f0b2dccd4072ca903fcdec8d9f499b4bd27
Sergey Reva <sergey.reva1@gmail.com>
2026-01-07 12:52:35 +0200

    clangformat

--- src/services/web/web.h
+++ src/services/web/web.h
@@ -3,7 +3,7 @@
 #include "keira/service.h"
 #include "services/network/network.h"
 
-#define WEB_BUFFER_FS_OP 4096
+#define WEB_BUFFER_FS_OP    4096
 #define WEB_BUFFER_FLASH_OP 4096
 
 class WebService : public Service {
