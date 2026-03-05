2ba8273f51df2b7af6121f417eee9e85416d1e9c
Alex <15167344+frostmorn@users.noreply.github.com>
2026-03-05 12:49:02 +0000

    Use more simple mutex debugage

--- src/keira/mutex.h
+++ src/keira/mutex.h
@@ -1,53 +1,24 @@
 #pragma once
-#include <FreeRTOS.h>
-#include <lilka/serial.h>
 
-// Mutex debug utils
+// Uncomment this line to get mutex debug log
 
-// Uncomment this line to get debug information about mutexes
-
-#define KEIRA_MUTEX_DEBUG
+//#define KEIRA_MUTEX_DEBUG
 
 #ifdef KEIRA_MUTEX_DEBUG
 
-#    define KMTX_MAX_FUNC_LEN 260
-
-typedef struct {
-    SemaphoreHandle_t* mtx;
-    char funcName[KMTX_MAX_FUNC_LEN];
-    size_t line;
-    size_t id;
-} KeiraDebugMtx_t;
-
-size_t keiraDebugMutexLastId = 0;
-extern std::vector<KeiraDebugMtx_t> keiraDebugMutexList;
-extern SemaphoreHandle_t keiraDebugMutexSemaphore;
-
-class AcquireMutex {
-public:
-    // Lock mutex, check on double lock
-    explicit AcquireMutex(SemaphoreHandle_t& mtx, const char* funcName, size_t line);
-    // Released normally
-    void release();
-    // Object is out of scope, here we detect if it was released correctly
-    ~AcquireMutex();
-
-private:
-    // indicates mutex isn't unlocked normally
-    bool releasedByUser = false;
-    // indicates if mutex was already locked
-    bool multiLock = false;
-
-    size_t mtxId = 0;
-};
-
-// unique variable name
-#    define KMTX_CONCAT_INNER(a, b) a##b
-#    define KMTX_CONCAT(a, b)       KMTX_CONCAT_INNER(a, b)
-#    define KMTX_LOCK(X)            AcquireMutex KMTX_CONCAT(kmtxDbgMutex_, __LINE__)(X, __PRETTY_FUNCTION__, __LINE__)
-#    define KMTX_UNLOCK(X)
+#    define KMTX_LOCK(X)                            \
+        {                                           \
+            lilka::serial.log("LOCKED %p mtx", &X); \
+            xSemaphoreTake(X, portMAX_DELAY);       \
+        }
+#    define KMTX_UNLOCK(X)                            \
+        {                                             \
+            lilka::serial.log("UNLOCKED %p mtx", &X); \
+            xSemaphoreGive(X);                        \
+        }
 
 #else
 #    define KMTX_LOCK(X)   xSemaphoreTake(X, portMAX_DELAY)
-#    define KMTX_UNLOCK(X) xSemaphoreGive(X)
+
+#    define KMTX_UNLOCK(X) xSemaphoreGive(X);
 #endif
