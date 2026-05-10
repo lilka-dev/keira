16559f0841d56293a9dcd14f57cd7005c89e9913
Alex <15167344+frostmorn@users.noreply.github.com>
2026-05-10 10:59:33 +0000

    Apply changes requested in alder review

--- tools/checklang.py
+++ tools/checklang.py
@@ -5,35 +5,31 @@
 ## across KeiraOS                                                               ##
 #                                                                                #
 ##################################################################################
-import os
+import os, re
+from pathlib import Path
 
-lang_files_path = "src/keira/localizations"
+LANG_FILES_PATH = Path("src") / "keira" / "localizations"
 
-lang_file_prefix = "lang_"
+LANG_FILE_PREFIX = "lang_"
 
-lang_main = "uk"
+LANG_FILE_MAIN = "uk"
 
-lang_main_filename = lang_files_path + "/" + lang_file_prefix + lang_main + ".h"
+LANG_FILE_RE = re.compile(r"^lang_[a-z]{2}\.h$")
 
-lang_files_list = []
-
-
-
+LANG_FILE_MAIN_NAME = LANG_FILES_PATH  / (LANG_FILE_PREFIX + LANG_FILE_MAIN + ".h")
 
 # Build a file list, skip non headers
-for filename in os.listdir(lang_files_path):
-	if filename.endswith(".h"):
-		lang_files_list.append(filename)
+lang_files_list = [p.name for p in LANG_FILES_PATH.glob("*.h")]
 
 # Check main language file existence
-if not((lang_file_prefix + lang_main+ ".h") in lang_files_list):
-	print("Error: Main language file for lang {} not found".format(lang_main))
+if not(f"{LANG_FILE_PREFIX}{LANG_FILE_MAIN}.h" in lang_files_list):
+	print(f"Error: Main language file for lang {lang_main} not found")
 	exit(-1)
 
 # Check filenames on a naming pattern
 for lang_file in lang_files_list:
-	if not(lang_file.startswith(lang_file_prefix) or (len(lang_file) == (len(lang_main) + len(lang_file_prefix) +2) )):
-		print("Error: Language file {} have incorrect name".format(lang_file))
+	if not(LANG_FILE_RE.match(lang_file)):
+		print(f"Error: Language file {lang_file} have incorrect name")
 		exit(-2)
 
 # Routine to read lang file and transfrom it in a list of definitions
@@ -50,7 +46,7 @@ def lang_file_read(lfile):
 	
 
 # Build definitions list
-token_list = lang_file_read(lang_main_filename)
+token_list = lang_file_read(LANG_FILE_MAIN_NAME)
 
 token_set = set(token_list)
 
@@ -58,9 +54,9 @@ error = 0
 
 for lang_file in lang_files_list:
 	# skip main file
-	if lang_file.endswith(lang_file_prefix+lang_main+".h"):
+	if lang_file.endswith(f"{LANG_FILE_PREFIX}{LANG_FILE_MAIN}.h"):
 		continue
-	set_cur = set(lang_file_read(lang_files_path+ "/" +lang_file))
+	set_cur = set(lang_file_read(LANG_FILES_PATH / lang_file))
 	set_diff = token_set-set_cur
 	if len(set_diff) > 0:
 		print("=====================================================")
