9e6a7bc40449210457288effcf766cc7c458f02f
Alex <15167344+frostmorn@users.noreply.github.com>
2026-06-04 16:50:12 +0300

    clang-format

--- src/apps/lilcatalog/lilcatalog.h
+++ src/apps/lilcatalog/lilcatalog.h
@@ -21,7 +21,6 @@
 #define CATALOG_ICON_HEIGHT 64
 #define CATALOG_ICON_SIZE   (CATALOG_ICON_WIDTH * CATALOG_ICON_HEIGHT * 2) // 8192 bytes
 
-
 // Base path
 #define CATALOG_FOLDER "/sd/lilcatalog"
 // Cache paths
